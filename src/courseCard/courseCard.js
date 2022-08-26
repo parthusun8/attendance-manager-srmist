055d566586cc8f580706a4d4c9bbcbed1a294e51
parthusun8 <parthusun8@gmail.com>
2022-08-26 13:42:29 +0530

    Card updated with vals

--- src/courseCard/courseCard.js
+++ src/courseCard/courseCard.js
@@ -8,6 +8,20 @@ function CourseCard({ course_info }) {
       <div className="course--details">
         <span>{course_info.subject_code}</span>
         <span>{course_info.subject_name}</span>
+        <div className="attendance--details">
+          <div className="small-dets">
+            <span>Total</span>
+            <span>{course_info.conducted_hours}</span>
+          </div>
+          <div className="small-dets">
+            <span>Absent</span>
+            <span>{course_info.absent_hours}</span>
+          </div>
+          <div className="small-dets">
+            <span>Present</span>
+            <span>{course_info.conducted_hours - course_info.absent_hours}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
